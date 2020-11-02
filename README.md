5c8909a98afe8600b3fc06473a110cffe4a00265
Razvan <axinie.razvan@gmail.com>
2020-11-02 16:41:59 +0200

    add code to remote

---
+++ README.md
@@ -0,0 +1,4 @@
+# GET Example
+
+A small example that illustrates how to perform a request from an API using QNetworkAccessManager.
+Requires Qt >= 5.0
---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <QCoreApplication>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QUrl>
+
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
+
+int main(int argc, char *argv[])
+{
+	QCoreApplication a(argc, argv);
+
+	QUrl url = QUrl("https://jsonplaceholder.typicode.com/todos");
+	QNetworkRequest request(url);
+
+	QNetworkAccessManager *manager = new QNetworkAccessManager();
+	QNetworkReply *reply = manager->get(request);
+
+	QObject::connect(reply, &QNetworkReply::finished, [=]() {
+		if (reply->error()) {
+			qDebug() << "Error reading data";
+			return;
+		}
+		QByteArray response = reply->readAll();
+		QJsonDocument doc = QJsonDocument::fromJson(response);
+		QJsonArray arr = doc.array();
+
+		// iterate over all elements of the received array
+		for(auto item : arr) {
+			QJsonObject obj = item.toObject();
+			// iterate over all object properties
+			for (auto key : obj.keys()) {
+				qDebug() << key << " ";
+				auto value = obj.value(key);
+
+				if (value.isString()) {
+					qDebug() << value.toString() << "\n";
+				} else if (value.isDouble()) {
+					qDebug() << value.toDouble() << "\n";
+				} else if (value.isBool()) {
+					qDebug() << value.toBool() << "\n";
+				}
+			}
+		}
+
+		delete manager;
+	});
+
+	return a.exec();
+}
